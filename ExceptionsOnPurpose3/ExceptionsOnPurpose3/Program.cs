735410c1e6a700c807cfa4cfa3d0a3d88e1c55a1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 08:38:59 -0600

    Update Program.cs

--- ExceptionsOnPurpose3/ExceptionsOnPurpose3/Program.cs
+++ ExceptionsOnPurpose3/ExceptionsOnPurpose3/Program.cs
@@ -6,7 +6,22 @@ namespace ExceptionsOnPurpose3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int answer = 0, result, zero = 0;
+            try
+            {
+                Console.Write("Enter an integer >> ");
+                answer = Convert.ToInt32(Console.ReadLine());
+                result = answer / zero;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("You did not enter an integer");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("This is not your fault.{0}You entered the integer correctly.{0}The program divides by zero.", Environment.NewLine);
+            }
+            Console.WriteLine("The answer is " + answer);
         }
     }
 }
