c4a9702d194dd76a16c379c12864c9b501243bb1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 09:13:00 -0600

    Update Program.cs

--- FindSquareRoot/FindSquareRoot/Program.cs
+++ FindSquareRoot/FindSquareRoot/Program.cs
@@ -6,7 +6,25 @@ namespace FindSquareRoot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isValidDouble = false;
+            double userDouble = 0, sqrRoot = 0;
+            while(!isValidDouble)
+            {
+                Console.Write("Enter a valid double: ");
+                isValidDouble = double.TryParse(Console.ReadLine(), out userDouble);
+            }
+            try
+            {
+                if (userDouble < 0) {
+                    throw new ApplicationException("Double can't be negative!");
+                } else {
+                    sqrRoot = Math.Sqrt(userDouble);
+                }
+            } catch (ApplicationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Square Root is " + sqrRoot);
         }
     }
 }
