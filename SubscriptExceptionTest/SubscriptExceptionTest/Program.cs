6a2a5d532babac7608721f28fe79a15fd3d12d37
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 08:46:14 -0600

    Update Program.cs

--- SubscriptExceptionTest/SubscriptExceptionTest/Program.cs
+++ SubscriptExceptionTest/SubscriptExceptionTest/Program.cs
@@ -6,7 +6,24 @@ namespace SubscriptExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] arr = new double[20];
+            try
+            {
+                for (int x = 0; x < arr.Length; ++x)
+                {
+                    Console.Write("Enter a number for position {0}: ", x);
+                    double.TryParse(Console.ReadLine(), out double result);
+                    arr[x] = result;
+                }
+            } catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("{0}Finished Array{0}", Environment.NewLine);
+            for(int x = 0; x < arr.Length; ++x)
+            {
+                Console.Write("{0, 6}", arr[x]);
+            }
         }
     }
 }
