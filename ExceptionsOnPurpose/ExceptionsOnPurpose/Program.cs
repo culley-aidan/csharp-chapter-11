28eac0371033f6ab2ed997da0f3554f6c402fcf7
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 08:28:32 -0600

    started

---
+++ ExceptionsOnPurpose/ExceptionsOnPurpose/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExceptionsOnPurpose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer, result, zero = 0;
+            Console.Write("Enter an integer >> ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            result = answer / zero;
+            Console.WriteLine("The answer is ", answer);
+        }
+    }
+}
