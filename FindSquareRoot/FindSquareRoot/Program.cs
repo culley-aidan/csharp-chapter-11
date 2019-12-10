f0dbce02ad33545577c61b80a9ce54359c4d0cc1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 09:07:12 -0600

    started

---
+++ FindSquareRoot/FindSquareRoot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindSquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
