c6617f0ea4c86b46a6143c04e2403e22f215dbc1
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 09:05:00 -0600

    Update Program.cs

--- MortgageApplication/MortgageApplication/Program.cs
+++ MortgageApplication/MortgageApplication/Program.cs
@@ -6,7 +6,26 @@ namespace MortgageApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Enter credit score: ");
+                int.TryParse(Console.ReadLine(), out int credit);
+                try {
+                    Console.WriteLine(IsEligible(credit) ? "Yes, Eligible" : "No, Not eligible.");
+                    break;
+                } catch (ArgumentException e) {
+                    Console.WriteLine("The credit score is invalid!");
+                }
+            }
+        }
+
+        static bool IsEligible(int creditscore)
+        {
+            if ((creditscore >= 300) && (creditscore <= 850)) {
+                return (creditscore >= 650);
+            } else {
+                throw new ArgumentException();
+            }
         }
     }
 }
