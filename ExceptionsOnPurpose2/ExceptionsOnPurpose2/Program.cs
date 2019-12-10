f4322a09ef5ee4bf1716fbe8bc8df37e364ac79a
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-10 08:33:58 -0600

    Update Program.cs

--- ExceptionsOnPurpose2/ExceptionsOnPurpose2/Program.cs
+++ ExceptionsOnPurpose2/ExceptionsOnPurpose2/Program.cs
@@ -6,7 +6,17 @@ namespace ExceptionsOnPurpose2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int answer = 0, result, zero = 0;
+            try
+            {
+                Console.Write("Enter an integer >> ");
+                answer = Convert.ToInt32(Console.ReadLine());
+                result = answer / zero;
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("The answer is " + answer);
         }
     }
 }
